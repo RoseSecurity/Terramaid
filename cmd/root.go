6a1e416087d5000a3bc94b0116663da594b7cdf3
Tom Morelly <tommorelly@gmail.com>
2024-08-01 10:25:17 +1000

    feat(envs): add configuration via env vars

--- cmd/root.go
+++ cmd/root.go
@@ -2,70 +2,88 @@ package cmd
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"os/exec"
 
 	"github.com/RoseSecurity/terramaid/internal"
+	"github.com/caarlos0/env/v11"
 	"github.com/spf13/cobra"
 )
 
-var (
-	workingDir   string
-	tfDir        string
-	tfPlan       string
-	tfBinary     string
-	output       string
-	direction    string
-	subgraphName string
-)
+var Version = "0.0.1"
+type opts struct {
+	WorkingDir   string `env:"WORKING_DIR" envDefault:"."`
+	TFDir        string `env:"TF_DIR" envDefault:"."`
+	TFPlan       string `env:"TF_PLAN"`
+	TFBinary     string `env:"TF_BINARY"`
+	Output       string `env:"OUTPUT" envDefault:"Terramaid.md"`
+	Direction    string `env:"DIRECTION" envDefault:"TD"`
+	SubgraphName string `env:"SUBGRAPH_NAME" envDefault:"Terraform"`
+}
 
-var RootCmd = &cobra.Command{
-	Use:          "terramaid",
-	Short:        "A utility for generating Mermaid diagrams from Terraform",
-	SilenceUsage: true,
-	Run: func(cmd *cobra.Command, args []string) {
-		var err error
-		if tfBinary == "" {
-			tfBinary, err = exec.LookPath("terraform")
-			if err != nil {
-				fmt.Printf("Error finding Terraform binary: %v\n", err)
-				os.Exit(1)
+func TerramaidCmd() *cobra.Command {
+	opts := &opts{}
+
+	// Parse Envs
+	if err := env.ParseWithOptions(opts, env.Options{Prefix: "TERRAMAID_"}); err != nil {
+		log.Fatalf("error parsing envs: %w", err)
+	}
+
+	cmd := &cobra.Command{
+		Use:          "terramaid",
+		Short:        "A utility for generating Mermaid diagrams from Terraform",
+		SilenceUsage: true,
+		SilenceErrors: true,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			if opts.TFBinary == "" {
+				tfBinary, err := exec.LookPath("terraform")
+				if err != nil {
+					return fmt.Errorf("error finding Terraform binary: %w", err)
+				}
+
+				opts.TFBinary = tfBinary
 			}
-		}
 
-		graph, err := internal.ParseTerraform(workingDir, tfBinary, tfPlan)
-		if err != nil {
-			fmt.Printf("Error parsing Terraform: %v\n", err)
-			os.Exit(1)
-		}
+			graph, err := internal.ParseTerraform(opts.WorkingDir, opts.TFBinary, opts.TFPlan)
+			if err != nil {
+				return fmt.Errorf("error parsing Terraform: %w", err)
+			}
 
-		// Convert the graph to a Mermaid diagram
-		mermaidDiagram, err := internal.ConvertToMermaid(graph, direction, subgraphName)
-		if err != nil {
-			fmt.Printf("Error converting to Mermaid: %v\n", err)
-			os.Exit(1)
-		}
+			// Convert the graph to a Mermaid diagram
+			mermaidDiagram, err := internal.ConvertToMermaid(graph, opts.Direction, opts.SubgraphName)
+			if err != nil {
+				return fmt.Errorf("error converting to Mermaid: %w\n", err)
+			}
 
-		// Write the Mermaid diagram to the specified output file
-		err = os.WriteFile(output, []byte(mermaidDiagram), 0644)
-		if err != nil {
-			fmt.Printf("Error writing to file: %v\n", err)
-			os.Exit(1)
-		}
-		fmt.Printf("Mermaid diagram successfully written to %s\n", output)
-	},
+			// Write the Mermaid diagram to the specified output file
+			if err := os.WriteFile(opts.Output, []byte(mermaidDiagram), 0o644); err != nil {
+				return fmt.Errorf("Error writing to file: %w\n", err)
+			}
+
+			fmt.Printf("Mermaid diagram successfully written to %s\n", opts.Output)
+
+			return nil
+		},
+	}
+
+	cmd.Flags().StringVarP(&opts.Output, "output", "o", opts.Output, "Output file for Mermaid diagram (env: TERRAMAID_OUTPUT)")
+	cmd.Flags().StringVarP(&opts.Direction, "direction", "r", opts.Direction, "Specify the direction of the flowchart (env: TERRAMAID_DIRECTION)")
+	cmd.Flags().StringVarP(&opts.SubgraphName, "subgraph-name", "s", opts.SubgraphName, "Specify the subgraph name of the flowchart (env: TERRAMAID_SUBGRAPH_NAME)")
+	cmd.Flags().StringVarP(&opts.TFDir, "tf-dir", "d", opts.TFDir, "Path to Terraform directory (env: TERRAMAID_TF_DIR)")
+	cmd.Flags().StringVarP(&opts.TFPlan, "tf-plan", "p", opts.TFPlan, "Path to Terraform plan file (env: TERRAMAID_TF_PLAN)")
+	cmd.Flags().StringVarP(&opts.TFBinary, "tf-binary", "b", opts.TFBinary, "Path to Terraform binary (env: TERRAMAID_TF_BINARY)")
+	cmd.Flags().StringVarP(&opts.WorkingDir, "working-dir", "w", opts.WorkingDir, "Working directory for Terraform (env: TERRAMAID_WORKING_DIR)")
+
+	cmd.AddCommand(docsCmd(), versionCmd())
+
+	return cmd
 }
 
 func Execute() error {
-	return RootCmd.Execute()
-}
+	if err := TerramaidCmd().Execute(); err != nil {
+		return err
+	}
 
-func init() {
-	RootCmd.Flags().StringVarP(&output, "output", "o", "Terramaid.md", "Output file for Mermaid diagram")
-	RootCmd.Flags().StringVarP(&direction, "direction", "r", "TD", "Specify the direction of the flowchart")
-	RootCmd.Flags().StringVarP(&subgraphName, "subgraphName", "s", "Terraform", "Specify the subgraph name of the flowchart")
-	RootCmd.Flags().StringVarP(&tfDir, "tfDir", "d", ".", "Path to Terraform directory")
-	RootCmd.Flags().StringVarP(&tfPlan, "tfPlan", "p", "", "Path to Terraform plan file")
-	RootCmd.Flags().StringVarP(&tfBinary, "tfBinary", "b", "", "Path to Terraform binary")
-	RootCmd.Flags().StringVarP(&workingDir, "workingDir", "w", ".", "Working directory for Terraform")
+	return nil
 }
--- main.go
+++ main.go
@@ -1,13 +1,20 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/RoseSecurity/terramaid/cmd"
-	u "github.com/RoseSecurity/terramaid/pkg/utils"
 )
 
+var version string
+
 func main() {
-	err := cmd.Execute()
-	if err != nil {
-		u.LogErrorAndExit(err)
+	cmd.Version = version 
+
+	if err := cmd.Execute(); err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %s\n", err)
+
+		os.Exit(1)
 	}
 }
