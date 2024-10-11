f3e25dda183dd5f9508adecfde006db72d94e25a
RoseSecurity <michaeljrosenfeld22@gmail.com>
2024-10-11 12:42:43 -0400

    Add Spinner

--- go.mod
+++ go.mod
@@ -4,9 +4,11 @@ go 1.23
 
 require (
 	github.com/awalterschulze/gographviz v2.0.3+incompatible
+	github.com/briandowns/spinner v1.23.1
 	github.com/caarlos0/env/v11 v11.2.2
 	github.com/fatih/color v1.17.0
 	github.com/hashicorp/terraform-exec v0.21.0
+	github.com/mattn/go-colorable v0.1.13
 	github.com/spf13/cobra v1.8.1
 	golang.org/x/text v0.17.0
 )
@@ -17,11 +19,11 @@ require (
 	github.com/hashicorp/go-version v1.7.0 // indirect
 	github.com/hashicorp/terraform-json v0.22.1 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
-	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/zclconf/go-cty v1.14.4 // indirect
 	golang.org/x/sys v0.18.0 // indirect
+	golang.org/x/term v0.1.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -1,24 +1,39 @@
 package utils
 
 import (
+	"fmt"
 	"os"
 	"path/filepath"
+	"time"
+
+	"github.com/briandowns/spinner"
+	"github.com/mattn/go-colorable"
 )
+
+const (
+	ColorReset     = "\033[0m"
+	ColorGreen     = "\033[32m"
+	ColorYellow    = "\033[33m"
+	ColorRed       = "\033[31m"
+	ColorBold      = "\033[1m"
+	ColorUnderline = "\033[4m"
+)
+
+type Spinner struct {
+	s *spinner.Spinner
+}
 
 // Check if a directory exists
 func DirExists(dir string) bool {
-	if _, err := os.Stat(dir); os.IsNotExist(err) {
-		return false
-	}
-
-	return true
+	_, err := os.Stat(dir)
+	return !os.IsNotExist(err)
 }
 
 // Check if Terraform files exist in a directory
-func TerraformFilesExist(dir string) bool {
+func TerraformFilesExist(dir string) (bool, error) {
 	validExtensions := []string{".tf", ".tf.json", ".tftest.hcl", ".tftest.json", "terraform.tfvars", "terraform.tfvars.json"}
 
-	found := false
+	var found bool
 	err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			return err
@@ -32,8 +47,28 @@ func TerraformFilesExist(dir string) bool {
 		return nil
 	})
 	if err != nil {
-		return false
+		return false, err
 	}
 
-	return found
+	return found, nil
+}
+
+// Initialize a new spinner
+func NewSpinner(text string) *Spinner {
+	s := spinner.New(spinner.CharSets[14], 100*time.Millisecond)
+	s.Color("blue")
+	s.Writer = colorable.NewColorableStdout() // Ensure colors are supported on Windows
+	s.Suffix = " " + text
+	return &Spinner{s: s}
+}
+
+// Start the spinner
+func (sp *Spinner) Start() {
+	fmt.Printf("%s%s%s ", ColorBold+ColorGreen, sp.s.Suffix, ColorReset)
+	sp.s.Start()
+}
+
+// Stop the spinner
+func (sp *Spinner) Stop() {
+	sp.s.Stop()
 }
