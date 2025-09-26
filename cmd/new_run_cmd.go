7213195659e614efbaf5a99b78ccf1bbfc403cd1
RoseSecurity <michael@rosesecurity.dev>
2025-09-26 10:55:49 -0400

    chore: add back test files

---
+++ cmd/new_run_cmd.go
@@ -0,0 +1,17 @@
+// Copyright (c) RoseSecurity
+// SPDX-License-Identifier: Apache-2.0
+
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
+)
+
+// NewRunCmd returns a pristine copy of runCmd so that each test/fuzz
+// iteration starts with clean flag state.
+func NewRunCmd() *cobra.Command {
+	clone := *runCmd // shallow‑copy the definition
+	clone.Flags().VisitAll(func(f *pflag.Flag) { f.Value.Set(f.DefValue) })
+	return &clone
+}
--- cmd/run.go
+++ cmd/run.go
@@ -1,3 +1,4 @@
+// Copyright (c) RoseSecurity
 // SPDX-License-Identifier: Apache-2.0
 
 package cmd
